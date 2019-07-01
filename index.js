1d03e4a57ea81c0d1844344c75d9300e4bcd7804
Blagojche Grujoski <b.grujoski@esportsconstruct.com>
2019-07-01 22:15:45 +0200

    fix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ mongoose.Promise = global.Promise;
 
 //Start server
 const server = app.listen(
-  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000,
+  process.env.PORT,
   function() {
     //Schedule giveaway every friday
     scheduleGiveaway();
