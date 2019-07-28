491830a787592e526c9a1ee2aea8dd9a9a58702e
Blagojche Grujoski <b.grujoski@esportsconstruct.com>
2019-07-28 11:55:01 +0200

    landings model update, XY position coords

--- models/Landing.js
+++ models/Landing.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
   id: { type: String, required: true },
-  place: { type: String, required: true }
+  place: { type: String, required: true },
+  position: {
+  	x: Number,
+  	y: Number
+  }
 });
 
 schema.set("toJSON", { virtuals: true });
